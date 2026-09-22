a3de88619fa685a9f7655b9afa1d229d2544831a
Tsvetelin Marinov <cvetelinmarinov566@gmail.com>
2026-09-22 16:49:43 +0300

    Added the core web mvc architecture of the app. All the ASP.NET MVC types and logic.

---
+++ Geography.Web/Program.cs
@@ -0,0 +1,24 @@
+/*
+ * Entry point of the application.
+ */
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.MapStaticAssets();
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}")
+   .WithStaticAssets();
+
+app.Run();
